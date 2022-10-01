8b2a909ca2e373e7f6183129df485c81e772eb7b
Christian Matos <chrismatos5001@gmail.com>
2022-10-01 13:51:31 -0400

    Initial Commit

---
+++ Lab_5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab_5
+//
+//  Created by Christian Matos on 10/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
